b996d20b06bb208de6e5fd7d0658426c7c3d3ca0
xyir4ik <mamulyluv@gmail.com>
2026-06-20 04:20:32 +0300

    fix sandboxed preload app version

--- preload.js
+++ preload.js
@@ -1,11 +1,13 @@
 const { contextBridge, ipcRenderer } = require('electron');
-const { version } = require('./package.json');
 
 const DEFAULT_API_BASE = 'https://resellcontrollbot.onrender.com';
 const env = typeof process === 'object' && process && process.env ? process.env : {};
+const argv = typeof process === 'object' && process && Array.isArray(process.argv) ? process.argv : [];
+const appVersionArg = argv.find(arg => arg.startsWith('--app-version='));
+const appVersion = appVersionArg ? appVersionArg.slice('--app-version='.length) : '0.0.0';
 
 contextBridge.exposeInMainWorld('desktopApi', Object.freeze({
-    appVersion: version,
+    appVersion,
     apiBase: env.API_BASE || DEFAULT_API_BASE,
     minimize: () => ipcRenderer.send('window:minimize'),
     close: () => ipcRenderer.send('window:close'),
